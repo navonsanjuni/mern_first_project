fc695687319a9ad49020bd79e658eafdb053e411
navonsanjuni <navonsanjuni178@gmail.com>
2025-05-07 15:55:14 +0530

    Creaate product model

---
+++ backend/models/product.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+
+    },
+    price:{
+        type: Number,
+        required: true,
+        
+    },
+    image:{
+        type:String,
+        required: true
+    },
+},{
+    timestamps: true
+});
+
+const Product = mongoose.model('Product', productSchema)
+//products is the name of the collection in the database 
+export default Product;
